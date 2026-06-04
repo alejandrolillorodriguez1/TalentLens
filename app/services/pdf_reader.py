86c50b16a6dbf4f0d0b65959bbe5fdbadc954bad
alejandrolillorodriguez1 <alejandrolillorod.alr@gmail.com>
2026-06-04 13:46:00 +0200

    modificacion en el pdf reader tras comprobar su funcionamiento

--- app/services/pdf_reader.py
+++ app/services/pdf_reader.py
@@ -4,5 +4,5 @@ def extract_text_pdf(path):
     reader = PdfReader(path)
     text = ""
     for pages in reader.pages : 
-        text += pages.extract_text() + "/n"
+        text += pages.extract_text() + "\n"
     return text
