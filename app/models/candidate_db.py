cf6ac36e346a4006e1f0c0614607a7bfe5742a95
alejandrolillorodriguez1 <alejandrolillorod.alr@gmail.com>
2026-06-18 11:27:17 +0200

    estructura de base de datos hecha usando el orm de fastapi

---
+++ app/models/candidate_db.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String,Text
+from app.db import base
+
+class Candidate(base):
+    __tablename__ = "candidates"
+
+    id = Column(Integer, primary_key=True, index=True)
+    cv_name = Column(String,nullable=False)
+    offer_name = Column(String, nullable=False)
+    candidate_skills = Column(Text, nullable=False)
+    required_skills = Column(Text, nullable=False)
+    score = Column(Integer, nullable=False)
+    decision = Column(String, nullable=False)
